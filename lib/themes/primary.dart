4f7303ed69a46c0d4390b910a2e5dc7fb0c63203
ned0emo <Alexandr42suv@mail.ru>
2024-06-13 10:23:50 +0800

    Обновление библиотек Исправление тем под новую версию flutter

--- lib/themes/primary.dart
+++ lib/themes/primary.dart
@@ -4,34 +4,37 @@ import 'package:schedule/themes/theme.dart';
 class PrimaryTheme extends ThemeTemplate {
   @override
   ThemeData get data => ThemeData(
-        colorScheme: ColorScheme.light(
-          primary: Colors.deepOrange,
-          surface: Colors.grey[100] ?? Colors.white,
-          background: Colors.grey[100] ?? Colors.white,
+    applyElevationOverlayColor: false,
+    canvasColor: Colors.grey[100],
+    colorScheme: ColorScheme.light(
+      primary: Colors.deepOrange,
+      surface: Colors.grey[50] ?? Colors.white,
+    ),
+    scaffoldBackgroundColor: Colors.grey[50],
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: const Color(0xFFFCF0EE),
+        foregroundColor: Colors.grey[850],
+        textStyle: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
         ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.grey[850],
-            textStyle: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ),
-        cardTheme: const CardTheme(surfaceTintColor: Colors.white),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.black87,
-          ),
-        ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.black,
-          ),
-        ),
-        tabBarTheme: const TabBarTheme(labelColor: Colors.black87),
-        snackBarTheme: SnackBarThemeData(
-          backgroundColor: Colors.grey[800],
-        ),
-      );
+      ),
+    ),
+    cardTheme: const CardTheme(surfaceTintColor: Colors.white, color: Colors.white),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: Colors.black87,
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        foregroundColor: Colors.black,
+      ),
+    ),
+    tabBarTheme: const TabBarTheme(labelColor: Colors.black87),
+    snackBarTheme: SnackBarThemeData(
+      backgroundColor: Colors.grey[800],
+    ),
+  );
 }
